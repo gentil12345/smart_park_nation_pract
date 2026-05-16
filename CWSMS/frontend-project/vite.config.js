5d8261c357885ad7d675586afcf163a27164fa56
gentil12345 <gentilgakiza9@gmail.com>
2026-05-16 01:18:55 -0700

    Update vite.config.js
    add

--- CWSMS/frontend-project/vite.config.js
+++ CWSMS/frontend-project/vite.config.js
@@ -3,15 +3,5 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    port: 5176,
-    proxy: {
-      "/api": {
-        target: "http://localhost:6000",
-        changeOrigin: true,
-        credentials: true,
-      },
-    },
-  },
+  plugins: [react(), tailwindcss()]
 });
